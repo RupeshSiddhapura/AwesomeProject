cf7417725d6a480946e2c605f6973c048fd89a30
RupeshSiddhapura <siddhapurarupesh@gmail.com>
2024-06-04 12:58:54 +0530

    new update

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const YourApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor:"magenta",
+      }}>
+      <Text>Hello rupesh🎉</Text>
+      <Text>Hello sanjay🎉</Text>
+      <Text>Hello ketan🎉</Text>
+      
+    </View>
+  );
+};
+
+export default YourApp;
--- App.tsx
+++ App.tsx
@@ -1,17 +1,6 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-
-const YourApp = () => {
-  return (
-    <View
-      style={{
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
       }}>
       <Text>Hello rupesh🎉</Text>
     </View>
-  );
-};
-
-export default YourApp;
